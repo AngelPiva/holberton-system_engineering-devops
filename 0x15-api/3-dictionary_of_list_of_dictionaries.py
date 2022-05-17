dfadccc30e4b2d1156348c38b6039672c311fc83
AngelPiva <angelpiva6@gmail.com>
2022-05-16 18:55:22 -0700

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -21,11 +21,12 @@ if __name__ == "__main__":
         dict = {}
         list = []
         for i in response_todos:
-            in_dict = {}
-            in_dict["task"] = i.get("title")
-            in_dict["completed"] = i.get("completed")
-            in_dict["username"] = u.get("username")
-            list.append(in_dict)
+            if i.get("userId") == u.get("id"):
+                in_dict = {}
+                in_dict["task"] = i.get("title")
+                in_dict["completed"] = i.get("completed")
+                in_dict["username"] = u.get("username")
+                list.append(in_dict)
         all_users[u.get("id")] = list
     with open(file_name, "w") as f:
         json.dump(all_users, f)
