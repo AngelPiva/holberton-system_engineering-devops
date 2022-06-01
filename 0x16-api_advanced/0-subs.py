bb99d436577d3ae6613c68a44683872f3b4bbdea
AngelPiva <angelpiva6@gmail.com>
2022-06-01 01:39:15 +0000

    fixed identation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,11 +4,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-	"""function"""
-	url = "https://www.reddit.com/r/" + subreddit + "/about.json"
-	user_agent = {'User-agent': 'Python/requests'}
-	req = requests.get(url, headers=user_agent, allow_redirects=False)
-	if req.status_code == 200:
-		return req.json().get("data").get("subscribers")
-	else:
-		return 0
+    """function"""
+    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+    user_agent = {'User-agent': 'Python/requests'}
+    req = requests.get(url, headers=user_agent, allow_redirects=False)
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
+        return 0
